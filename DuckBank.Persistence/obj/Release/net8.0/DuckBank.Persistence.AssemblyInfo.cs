81121e4480145fee22bef552af8b988626ff41ee
Víctor Martinez <ahal_tocob@hotmail.com>
2025-03-31 18:47:34 -0600

    Flujo de apis completado

--- DuckBank.Persistence/obj/Release/net8.0/DuckBank.Persistence.AssemblyInfo.cs
+++ DuckBank.Persistence/obj/Release/net8.0/DuckBank.Persistence.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuckBank.Persistence")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edea3cb230cbbf7d1f8556360c5050b8806c810")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuckBank.Persistence")]
