13725f08c186e1c97fdfc9db82f7f4b4b43f498b
Víctor Martinez <ahal_tocob@hotmail.com>
2025-04-25 19:14:30 -0600

    Version Rc

--- DuckBank.Persistence/Interfaces/IAhorroRepositorio.cs
+++ DuckBank.Persistence/Interfaces/IAhorroRepositorio.cs
@@ -7,7 +7,14 @@ namespace DuckBank.Persistence.Interfaces
         Task ActualizarAsync(Ahorro ahorro);
         Task<int> AgregarAsync(Ahorro item);
         Task<int> DepositarAsync(string idGuid, Movimiento movimiento);
-        Task<int> RetirarAsync(string idGuid, Movimiento movimiento);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ahorroId"></param>
+        /// <param name="movimiento"></param>
+        /// <returns></returns>
+        Task<int> RetirarAsync(string ahorroId, Movimiento movimiento);
         Task<List<Ahorro>> ObtenerAsync();        
         Task<List<Ahorro>> ObtenerListaDeAhorrosPorClienteIdAsync(string clienteId);       
         Task<Ahorro> ObtenerPorIdAsync(string idGuid);        
